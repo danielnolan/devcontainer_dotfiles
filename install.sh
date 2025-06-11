719e99842c163d1c8c7f676f6cb827b4bc3835ad
Daniel Nolan <danielanolan@gmail.com>
2025-06-10 22:56:14 -0400

    Add install file

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# this file is for installing dotfiles in devcontainer/codespace
+if [ -z "$USER" ]; then
+  USER=$(id -un)
+fi
+
+cd $HOME
+
+# remove oh my zsh
+~/.oh-my-zsh/tools/uninstall.sh
+rm .zshrc
+rm .zshenv
+rm .zprofile
+
+# Make passwordless sudo work
+export SUDO_ASKPASS=/bin/true
+
+# Install ripgrep for fast searching
+sudo apt-get install ripgrep
+
+# Install yadm for managing dotfiles
+sudo apt-get update
+sudo apt-get install -y yadm
+yadm clone -f https://github.com/danielnolan/dotfiles
