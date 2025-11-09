f9a79a398e57b66c98d75345bae52b23f41bf82f
Daniel Nolan <danielanolan@gmail.com>
2025-11-08 20:50:15 -0500

    Use yadm bootstrap
    yadm has the ability to run bootstrap scripts. Let's make use of that.

--- install.sh
+++ install.sh
@@ -9,8 +9,6 @@ cd $HOME
 
 # remove oh my zsh
 rm -rf .oh-my-zsh
-rm .zshrc
-rm .zprofile
 
 # Make passwordless sudo work
 export SUDO_ASKPASS=/bin/true
@@ -18,15 +16,11 @@ export SUDO_ASKPASS=/bin/true
 # Update apt package cache
 sudo apt-get update
 
-# Install ripgrep for fast searching
-sudo apt-get install ripgrep
-
 # Install yadm for managing dotfiles
 sudo apt-get install -y yadm
-yadm clone -f https://github.com/danielnolan/dotfiles.git
+yadm clone -f https://github.com/danielnolan/dotfiles.git --bootstrap
 yadm sparse-checkout set --no-cone '/*' '!README.md' '!LICENSE'
 yadm remote set-url origin git@github.com:danielnolan/dotfiles.git
-
 rm -rf dotfiles
 
 # ensure zsh is still default shell
